0850d35d1bc0a157c2c4496f2610951d046a1edd
MollyLin <lin.zy.molly@gmail.com>
2013-10-27 16:27:05 -0400

    Changes to see if DB update works

--- DesignProject/src/AddOption.java
+++ DesignProject/src/AddOption.java
@@ -4,10 +4,13 @@ public class AddOption {
 	
 	public static void AddToOption(String User, String OptionName, String OptionCareer) {
 		System.out.println("Hello World.");
-		String X ="";
+		//String X ="";
 		JOptionPane.showMessageDialog(null, "Do you want to add "+ OptionName+" to your option list");
 		
-		int test;
+		//if 
+		//int test;
+		
+		
 		
 		
 		
