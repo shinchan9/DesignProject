5722c501568b22fc883ebc10359b3207f0b681eb
shinchan9 <sseddiehong@hotmail.com>
2013-11-03 13:49:32 -0500

    Added features on ResourceAvaliablity.java

--- DesignProject/src/ResourceAvailability.java
+++ DesignProject/src/ResourceAvailability.java
@@ -1,13 +1,58 @@
+import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PGraphics;
+import processing.core.PGraphicsJava2D;
 import javax.swing.JOptionPane;
+import controlP5.*; 
 
-public class ResourceAvailability {
+public class ResourceAvailability extends PApplet{
 	
-	public static void main(){
+PFont f;
+String typing = "";
+String saved = "";
+	
+	public void setup(){
 		//ResourceAvailability
+		size(500,500);
+		f = createFont ("Arial", 16,true);
+
+	}
+	public void draw() {
 		
+		background(255);
+		int indent = 25;
 		
+		textFont(f);
+		fill(0);
+		
+		//display
+		text("If I could have any career I would choose ____", indent,40);
+		text(typing, indent,90);
+		text(saved, indent,130);
+	}
+		
+//		stroke(255);
+//		if (mousePressed) {
+//			line(mouseX, mouseY,pmouseX,pmouseY);
+//		}
+		
+	
+	public void keyPressed() {
+		
+		if (key == '\n') {
+			saved = typing;
+			// A String can be cleared by setting it equal to ""
+			typing = "";
+					
+		} else {
+			// otherwise, concatenate the String
+			// Each character typed by the user is added to the end of the string variable
+			typing = typing + key;
+		
+		}
 		
 	}
+	
 	
 	
 
